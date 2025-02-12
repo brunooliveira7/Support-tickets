56e596220575f7d18f77df1711c1e8b24809e87f
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-12 20:54:42 -0300

    Route Middleware

---
+++ src/middlewares/routeHandler.js
@@ -0,0 +1,17 @@
+import { routes } from "../routes/index.js";
+
+export function routeHandler(request, response) {
+  //procura a rota
+  const route = routes.find((route) => {
+    //se a rota tem método, path e requisição iguais retorna rota
+    return route.method === request.method && route.path === request.url;
+  });
+
+  if (route) {
+    //se a rota existe, executa o controller
+    return route.controller(request, response);
+  } else {
+    //se não existe, retorna 404
+    return response.writeHead(404).end();
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import http from "node:http";
 
 import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 async function listener(request, response) {
   //adiciona o middleware jsonHandler - obtém o corpo da requisição
   await jsonHandler(request, response);
-  console.log(request.body);
+  
+  //adiciona o middleware routeHandler - executa a rota
+  routeHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);

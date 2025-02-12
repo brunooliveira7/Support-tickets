4bbc427ebf8410e86796fd589b69bdce0da3b937
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-12 20:32:01 -0300

    creating a list of routes

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+//juntar todas as rotas
+import { tickets } from "./tickets.js";
+
+//despeja todas as rotas em um array
+export const routes = [...tickets];
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import http from "node:http";
 import { jsonHandler } from "./middlewares/jsonHandler.js";
 
 async function listener(request, response) {
+  //adiciona o middleware jsonHandler - obtém o corpo da requisição
   await jsonHandler(request, response);
   console.log(request.body);
 }

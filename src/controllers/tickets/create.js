0df6f4a53728e8fedb5243f29df5d7c7759d0df8
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-13 20:47:23 -0300

    creating the ticket object

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,20 @@
+//impota da biblioteca do node um gerador de id aleatórios
+import { randomUUID } from "node:crypto";
+
 export function create({ request, response }) {
-  return response.end("Criado com sucesso!");
+  //recuperar o corpo da requisição
+  const { equipment, description, user_name } = request.body;
+
+  ///criar um objeto com os dados da requisição
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  return response.end(JSON.stringify(ticket));
 }

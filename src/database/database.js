6da58ea71a5a430b9083964485d6d8d313cffcd1
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-19 20:27:35 -0300

    removing a ticket

--- src/database/database.js
+++ src/database/database.js
@@ -67,7 +67,17 @@ export class Database {
         ...this.#database[table][rowIndex],
         ...data,
       };
-      
+
+      this.#persist();
+    }
+  }
+
+  //método para deletar dados do banco de dados
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
       this.#persist();
     }
   }
